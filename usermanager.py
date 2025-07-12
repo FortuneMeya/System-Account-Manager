0466440c9419650c9e046b97afabced856a1b5b8
FortuneMeya <fortunemeya@gmail.com>
2025-07-11 21:03:01 -0700

    -Added and finished register_user method -Finished with the usermanager.py

--- usermanager.py
+++ usermanager.py
@@ -1,19 +1,24 @@
 import json
+
+import bcrypt
+
+
 class UserManager:
-    def __init__(self,file):
+    def __init__(self,file,hasher):
         self.file = file
+        self.hasher= hasher
 
 
-    def register_user(self,username,password,hasher):
+    def register_user(self,username,password):
         try:
 
-             with open("usernames.json","r") as f:
+             with open(self.file,"r") as f:
                  data=json.load(f)
         except FileNotFoundError:
             data= {}
         if username in data:
                 return False
-        hashed_pw= hasher.hash_password(password)
+        hashed_pw= self.hasher.hash_password(password)
         data[username] = hashed_pw.decode()
 
         with open(self.file,"w") as f:
@@ -21,14 +26,17 @@ class UserManager:
 
         return True
 
-
-
-
-
-
-
-
-
-
-
-
+    def validate_user(self,username,password):
+        try:
+            with open(self.file,"r") as f:
+                data=json.load(f)
+        except FileNotFoundError:
+            data={}
+        if username not in data:
+            return False
+        stored_hash = data[username]
+        try:
+             result= bcrypt.checkpw(password.encode(),stored_hash.encode())
+             return result
+        except (TypeError,ValueError):
+             return False
